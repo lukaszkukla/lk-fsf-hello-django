48884b0827b80978914d534150a20ebad0af5439
lukaszkukla <lukaszkukla@gmail.com>
2022-06-21 22:42:38 +0000

    Feat: add database and record

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from tomlkit import item
+from .models import Item
 
 # Register your models here.
+admin.site.register(Item )
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model): # inerhits all what is built-in Django model class
+    name = models.CharField(max_length=50, null=False, blank=False)
+    done = models.BooleanField(null=False, blank=False, default=False)
+
+    def __str__(self):
+        return self.name 
